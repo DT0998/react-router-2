9373680c72fe3438c03141a3e2963ace5fd04863
DT0998 <duongkytrieuvn@gmail.com>
2022-06-20 16:47:55 +0700

    update quotes route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { Route, Switch } from "react-router-dom";
+import AllQuotes from "./pages/AllQuotes";
+import NewQuotes from "./pages/NewQuotes";
+import QuotesDetails from "./pages/QuotesDetail";
+
 function App() {
   return (
-    <div>
-
-    </div>
+    <Switch>
+      <Route exact path="/quotes">
+        <AllQuotes />
+      </Route>
+      <Route path="/quotes/:quoteId">
+        <QuotesDetails />
+      </Route>
+      <Route path="/new-quote">
+        <NewQuotes />
+      </Route>
+    </Switch>
   );
 }
 
---
+++ src/pages/QuotesDetail.js
@@ -0,0 +1,7 @@
+function QuotesDetails() {
+    return (
+        <h1>Quotes Details</h1>
+      );
+}
+
+export default QuotesDetails;
